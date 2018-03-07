e3dff39babd3adf702ee454810098d927ce19802
hudik <hudikm@gmail.com>
2018-03-07 15:40:15 +0100

    finalna verzia

--- app/src/main/java/fri/uniza/sk/cookbookexample/MainActivity.java
+++ app/src/main/java/fri/uniza/sk/cookbookexample/MainActivity.java
@@ -1,8 +1,12 @@
 package fri.uniza.sk.cookbookexample;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,7 +16,11 @@ import fri.uniza.sk.cookbookexample.model.Recipe;
 import fri.uniza.sk.cookbookexample.model.Singleton;
 import fri.uniza.sk.cookbookexample.utilities.LoadData;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RecipeFragment.OnListFragmentInteractionListener {
+
+    private View detailFragmentContainer = null;
+    private DetailFragment detailFragmentl = null;
+    private int curentPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +37,57 @@ public class MainActivity extends AppCompatActivity {
 
         } catch (IOException e) {
             e.printStackTrace();
+            Log.e("CookBook", "Data read error");
         }
+
+        detailFragmentContainer = findViewById(R.id.detailFragment);
+
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.listFrameLayout, RecipeFragment.newInstance());
+        //fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
+        Fragment detailFragment = getSupportFragmentManager().findFragmentByTag("detailFragment");
+        if (savedInstanceState != null && (detailFragmentContainer != null || detailFragment != null)) {
+            //
+            getSupportFragmentManager().popBackStack("detailFragment", FragmentManager.POP_BACK_STACK_INCLUSIVE);
+            int position = savedInstanceState.getInt("detailPosition");
+            createFragment(position);
+        }
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("detailPosition", curentPosition);
+    }
+
+    @Override
+    public void onListFragmentInteraction(Recipe item, int position) {
+
+        createFragment(position);
+
+
+    }
+
+    private void createFragment(int position) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        DetailFragment detailFragment = DetailFragment.newInstance(position);
+        curentPosition = position;
+        if (detailFragmentContainer == null) {
+
+            fragmentTransaction.replace(R.id.listFrameLayout, detailFragment, "detailFragment");
+            fragmentTransaction.addToBackStack("detailFragment");
+            fragmentTransaction.commit();
+
+        } else {
+
+            fragmentTransaction.replace(R.id.detailFragment, detailFragment, "detailFragment");
+            //fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        }
+
     }
 }
